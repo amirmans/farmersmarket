885cb24d033902b7bede3c96a670f29d9cb191cf
Amir Amirmansoury <amiramirmansoury@MyTigi.local>
2014-07-30 17:02:22 -0700

    trying to get back NotificationTableViewCell

---
+++ MainSourceCode/servicesTableViewCell.h
@@ -0,0 +1,16 @@
+//
+//  ServicesTableViewCell.h
+//  TapForAll
+//
+//  Created by Amir on 7/30/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ServicesTableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *iconImage;
+
+@property (weak, nonatomic) IBOutlet UITextView *serviceTextView;
+
+@end
---
+++ MainSourceCode/servicesTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  ServicesTableViewCell.m
+//  TapForAll
+//
+//  Created by Amir on 7/30/14.
+//
+//
+
+#import "ServicesTableViewCell.h"
+
+@implementation ServicesTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
